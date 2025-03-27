e6009352168711c16de85113f353f354eca28a83
saikeerthi0323 <saikeerthi780@gmail.com>
2025-03-27 12:13:00 +0530

    quiz

---
+++ src/Data/Questions.jsx
@@ -0,0 +1,54 @@
+const quizData = [
+    {
+        question: 'What is the capital of United Kingdom?',
+        options: ['London', 'Paris', 'England', 'France'],
+        answer: 'London'
+    },
+    {
+        question: 'Who is the Prime Minister of United Kingdom ?',
+        options: ['rishi sunak', 'liz truss', 'boris johnson', 'theresa may'],
+        answer: 'rishi sunak'
+    },
+    {
+        question: 'Which is the national animal of United kingdom?',
+        options: ['Tiger', 'Lion', 'Elephant', 'Leopard'],
+        answer: 'lion'
+    },
+    {
+        question: 'Which is the national bird of United kingdom?',
+        options: ['europesn robin', 'Sparrow', 'Pigeon', 'Eagle'],
+        answer: 'european robin'
+    },
+    {
+        question: 'Which is the national sport of United kingdom?',
+        options: ['Cricket', 'Hockey', 'Football', 'Kabaddi'],
+        answer: 'football'
+    },
+    {
+        question: 'Who is the current monarch of united kingdom?',
+        options: ['queen elizabeth', 'king charles', 'prince william', 'king george'],
+        answer: 'queen elizabeth'
+    },
+    {
+        question: 'Which is the longest river in the united kingdom',
+        options: ['severn', 'thames', 'clyde', 'mersy'],
+        answer: 'severn'
+    },
+    {
+        question: 'In which year did the united kingdom vote to leave the european union?',
+        options: ['2016', '2012', '2007', '2010'],
+        answer: '2016'
+    },
+    {
+        question: 'Which of these is the official currency of united kingdom?',
+        options: ['euro', 'pound sterling', 'dollar', 'franc'],
+        answer: 'pound sterling'
+    },
+    {
+        question: 'Which of these countries is not part of united kingdom?',
+        options: ['england', 'scotland', 'wales', 'ireland'],
+        answer: 'ireland'
+    }
+];
+
+export default quizData;
